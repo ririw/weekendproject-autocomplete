aa00f9518967198c1a9997fbadec2309ba5fdc8b
Richard Weiss <richardweiss@richardweiss.org>
2014-06-15 16:29:10 +1000

    Various small cleanups in countingTrie. Documentation mainly

--- src/main/scala/autocomplete/buc/CountingTrie.scala
+++ src/main/scala/autocomplete/buc/CountingTrie.scala
@@ -1,6 +1,5 @@
 package autocomplete.buc
 
-import scala.annotation.tailrec
 import scala.collection.{mutable, MapLike}
 
 /**
@@ -22,6 +21,16 @@ import scala.collection.{mutable, MapLike}
  * It operates on lists of prefixes, because their (item :: item :: Nil)
  * structure matches the recursive insertion operations done internally.
  *
+ * One might ask why we do the BucComputation when we've already done this.
+ * After all, all we need to do to make the BUC result is to trim the
+ * trie and lop off any branch with less than minSup items.
+ *
+ * The advantage the BUC implementation has is that it can be applied
+ * to many different things, as long as they have the query method defined.
+ *
+ * That said, maybe it's worth writing a clever BUCComputation that
+ * works by taking branches off the trie of counts
+ *
  * @param items a list of items to insert into the trie
  * @tparam PrefixKey the type for each item in the prefix.
  */
@@ -29,7 +38,11 @@ class CountingTrie[PrefixKey](items: Iterator[PrefixItem[PrefixKey]]) {
   private val heads: mutable.HashMap[PrefixKey, MTrieNode] = new mutable.HashMap[PrefixKey, MTrieNode]()
   items.foreach(item => addKey(item.item))
 
-  def addKey(key: List[PrefixKey]) {
+  /**
+   * Add an item to the mutable heads set of tries.
+   * @param key the key to add
+   */
+  private def addKey(key: List[PrefixKey]) {
     assert(key.length > 0)
     val child = heads.get(key.head)
     val nextNode = child match {
@@ -61,7 +74,7 @@ class CountingTrie[PrefixKey](items: Iterator[PrefixItem[PrefixKey]]) {
   }
 
   /**
-   * Get the counts for all the direct children, ie, only direct decendants
+   * Get the counts for all the direct children, ie, only direct descendants
    * @param key the key to get
    * @return a map from the full prefix item to
    */
@@ -74,10 +87,14 @@ class CountingTrie[PrefixKey](items: Iterator[PrefixItem[PrefixKey]]) {
         map{node => node.children.map{case (k, v) => PrefixItem(key.item ++ List(k)) -> v.count}}.getOrElse(Map())
     }
   }
-  @inline
-  def compareNode(node: TrieNode[_], to: PrefixKey): Boolean = node.key == to
 
-  @tailrec
+  /**
+   * "Forcibly" traverse a trie. This will always succeed, creating a node
+   * if one does exist, and also incrementing all the nodes along the path
+   * to the one created or returned.
+   * @param fromNode the node to start from. This only works on mutable nodes, for obvious reasons
+   * @param prefix the prefix to create/increment.
+   */
   private def traverseToForciblyAndIncrement(fromNode: MTrieNode, prefix: List[PrefixKey]) {
     prefix.isEmpty match {
       case true => Unit
@@ -95,8 +112,16 @@ class CountingTrie[PrefixKey](items: Iterator[PrefixItem[PrefixKey]]) {
     }
   }
 
-  @tailrec
-  private def traverseToGently[N <: TrieNode[N]](fromNode: N, prefix: List[PrefixKey]): Option[N] = {
+  /**
+   * "Gently traverse" a trie, meaning that the trie is not modified (compare to traverseToForciblyAndIncrement)
+   *
+   * Try to keep this tail-recursive, for speed.
+   * @param fromNode the node to start from
+   * @param prefix the prefix to search for
+   * @tparam N the type of the TrieNode
+   * @return a node, of type N, that is at that prefix address, or None if there is no such node
+   */
+  protected def traverseToGently[N <: TrieNode[N]](fromNode: N, prefix: List[PrefixKey]): Option[N] = {
     prefix.isEmpty match {
       case true => Some(fromNode)
       case false =>
@@ -108,17 +133,25 @@ class CountingTrie[PrefixKey](items: Iterator[PrefixItem[PrefixKey]]) {
     }
   }
 
-  trait TrieNode[T <: TrieNode[T]] {
+  /**
+   * All trie nodes share a few things:
+   * a key, and their children. Note that in
+   * MTrieNode, children is mutable.
+   * @tparam T a self type.
+   */
+  sealed trait TrieNode[T <: TrieNode[T]] {
     val key: PrefixKey
     val children: MapLike[PrefixKey, T, _]
   }
+
+  /** This is a mutable trie node. */
   case class MTrieNode(key: PrefixKey, var count: Long = 0l, children: mutable.HashMap[PrefixKey, MTrieNode]) extends TrieNode[MTrieNode] {
+    /** The freeze method converts this, and all its children, into FTrieNodes */
     def freeze(): FTrieNode = FTrieNode(key, count, children.map{case (k, v) => k -> v.freeze()}.toMap)
-    lazy val childrenMap = children.toMap
   }
-  case class FTrieNode(key: PrefixKey, count: Long, children: Map[PrefixKey, FTrieNode]) extends TrieNode[FTrieNode] {
-    lazy val childrenMap = children
-  }
+
+  /** This is a Fixed node in the trie, it is immutable */
+  case class FTrieNode(key: PrefixKey, count: Long, children: Map[PrefixKey, FTrieNode]) extends TrieNode[FTrieNode]
 }
 
 case class PrefixItem[A](item: List[A]) extends AnyVal
